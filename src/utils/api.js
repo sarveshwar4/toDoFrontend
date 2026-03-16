066b8875e86b72dcae43ed1c8eeb9f1718f40757
Sarveshwar Kumar Shukla <sarveshwarkumarshukla@gmail.com>
2026-03-16 20:58:54 +0530

    fix some problems

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://todobackend-9cbf.onrender.com",
-  // baseURL: "http://localhost:4444",
   withCredentials: true
 });
 
 export default API;
+
+  // baseURL: "http://localhost:4444",
